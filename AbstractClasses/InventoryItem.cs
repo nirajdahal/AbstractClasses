ca0b349c3231331759525056b7ff77cf3bdae59a
nirajdahal09@gmail.com <nirajdahal09@gmail.com>
2020-07-31 21:15:22 +0545

    Add project files.

---
+++ AbstractClasses/InventoryItem.cs
@@ -0,0 +1,9 @@
+﻿namespace AbstractClasses
+{
+    public abstract class InventoryItem
+    {
+        public string ProductName { get; set; }
+
+        public int QuantityInStock { get; set; }
+    }
+}
---
+++ AbstractClasses/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AbstractClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+    public abstract class Vehicle 
+    {
+        public string VIN { get; set; }
+
+        public string Manufacturer { get; set; }
+
+        public int YearOfManufacture { get; set; }
+
+    }
+
+    public class Car : Vehicle
+    {
+        public int NumberOfWheels { get; set; }
+    }
+}
